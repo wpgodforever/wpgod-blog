9844bd612a18daf035e0efb31df0d255e6703f18
wuping <wuping@xinli001.com>
2023-02-27 18:34:39 +0800

    [add]识别@引入配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    // 别名
+    alias: {
+      "@": path.join(__dirname, "./src"),
+    },
+    // extensions: [".ts", ".js"],
+  },
 })
