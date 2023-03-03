1146182fd1e21fc21c089f63b95dd9150fe2e247
wpgodforever <997587729@qq.com>
2023-03-03 14:12:27 +0800

    [add]vue3控制台警告解决

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <headNav></headNav>
-    <keep-alive>
-      <router-view v-if="$route.meta.keepAlive" />
-    </keep-alive>
-    <transition>
-      <router-view v-if="!$route.meta.keepAlive" />
-    </transition>
+    <router-view v-slot="{ Component }">
+      <!-- //动画效果 -->
+      <transition>
+        <!-- //需要缓存 -->
+        <keep-alive>
+          <component :is="Component" v-if="$route.meta.keepAlive" :key="$route.path" />
+        </keep-alive>
+      </transition>
+      <!-- //不需要缓存 -->
+      <component :is="Component" v-if="!$route.meta.keepAlive" :key="$route.path" />
+    </router-view>
   </div>
 </template>
 <script lang='ts' setup name="index">
 import { ref, reactive } from 'vue'
 import headNav from '@/components/headNav/headNav.vue';
 </script>
-<style scoped lang="less">
-
-</style>
+<style scoped lang="less"></style>
