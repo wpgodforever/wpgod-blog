5fe42daa65736d3101d36a0a416168e1d286001b
wpgodforever <997587729@qq.com>
2023-03-06 15:06:32 +0800

    [add]文章路由相关模块新增

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-const Index = () => import('../views/index/index.vue')
+const Index = () => import('../views/layout/index.vue')
 const Main = () => import('../views/main/index.vue')
+const ArticleEdit = () => import('../views/article/edit.vue')
+const ArticleDetail = () => import('../views/article/detail.vue')
 const routes: Array<RouteRecordRaw> = [
     { 
         path: '/', 
@@ -10,6 +12,20 @@ const routes: Array<RouteRecordRaw> = [
             {
                 path:'/index',
                 component: Main,
+            },
+            {
+                path:'/article',
+                redirect: '/article/detail',
+                children:[
+                    {
+                        path:'/article/edit',
+                        component: ArticleEdit
+                    },
+                    {
+                        path:'/article/detail',
+                        component: ArticleDetail
+                    },
+                ]
             }
         ] 
     },
---
+++ src/views/article/edit.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    发布文章
+  </div>
+</template>
+<script lang='ts' setup>
+import { ref, reactive } from 'vue'
+</script>
+<style scoped lang='less'>
+ 
+</style>
