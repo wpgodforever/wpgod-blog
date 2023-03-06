1da775523b8dee36030bdce6f756e60565f6784d
wpgodforever <997587729@qq.com>
2023-03-06 16:33:14 +0800

    [add]引入md-editor-v3编辑器

--- src/views/article/edit.vue
+++ src/views/article/edit.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-    发布文章
+  <div class="editor-box">
+    <md-editor v-model="text" />
+
   </div>
 </template>
 <script lang='ts' setup>
 import { ref, reactive } from 'vue'
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+const text = ref('Hello Editor!');
 </script>
 <style scoped lang='less'>
- 
+ .editor-box{
+   margin-top: 30%;
+ }
 </style>
