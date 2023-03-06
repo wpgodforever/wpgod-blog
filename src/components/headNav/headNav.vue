07284b8e26169eae405a5d141d8d297db41c68bf
wpgodforever <997587729@qq.com>
2023-03-06 10:49:33 +0800

    【add】顶部导航鼠标移入动画

--- src/components/headNav/headNav.vue
+++ src/components/headNav/headNav.vue
@@ -4,12 +4,12 @@
         <el-input v-model="searchVal" style="width: 240px;" class="ml-auto" placeholder="Type something"
             :prefix-icon="Search" />
         <div class="nav-list flex-align">
-            <div class="nav-list_item">首页</div>
-            <div class="nav-list_item">归档</div>
-            <div class="nav-list_item">面试</div>
-            <div class="nav-list_item" v-if="!userInfo.token"
+            <div class="nav-list_item" @mouseleave="hoverAnimationLeave" @mouseenter="hoverAnimation($event)">首页</div>
+            <div class="nav-list_item" @mouseleave="hoverAnimationLeave" @mouseenter="hoverAnimation($event)">归档</div>
+            <div class="nav-list_item" @mouseleave="hoverAnimationLeave" @mouseenter="hoverAnimation($event)">面试</div>
+            <div class="nav-list_item" @mouseleave="hoverAnimationLeave" @mouseenter="hoverAnimation($event)" v-if="!userInfo.token"
             @click="popClick(0)">注册</div>
-            <div class="nav-list_item" v-if="!userInfo.token" @click="popClick(1)">登录</div>
+            <div class="nav-list_item" @mouseleave="hoverAnimationLeave" @mouseenter="hoverAnimation($event)" v-if="!userInfo.token" @click="popClick(1)">登录</div>
         </div>
     </div>
     <loginPop v-model="dialogVisible" :tips="tips"></loginPop>
@@ -42,6 +42,15 @@ const popClick = (index:0|1) => {
 // 获取pinia用户数据
 const user = useUserStore()
 const { userInfo } = user
+
+
+// 鼠标移入按钮动画
+const hoverAnimation = ($event) => {
+    $event.currentTarget.className = 'nav-list_item animate__animated animate__bounce'
+}
+const hoverAnimationLeave = ($event) => {
+    $event.currentTarget.className = 'nav-list_item'
+}
 </script>
 <style scoped lang='less'>
 .headNav {
