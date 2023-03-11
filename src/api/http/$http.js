d62f386c2cf45d5936af38493c07e76456bdeff5
wpgodforever <997587729@qq.com>
2023-03-11 23:51:31 +0800

    生产环境开发环境区分baseurl

--- src/api/http/$http.js
+++ src/api/http/$http.js
@@ -12,6 +12,7 @@ axios.defaults.withCredentials = true
 const service = axios.create({
   // 用了proxy不用配置baseurl了
   // axios中请求配置有baseURL选项，表示请求URL公共部分
+  // baseURL: process.env.VUE_APP_BASE_URL,
   baseURL: '/api',
   withCredentials: false, // send cookies when cross-domain requests
   timeout: 60000,
