be0602cd1fb8ca98d699cdb572d83c220b723ea3
wpgodforever <997587729@qq.com>
2023-03-14 17:05:28 +0800

    [add]时间线模块基础样式

---
+++ src/views/timeLine/components/timeItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="itemContainer flex-col" :class="[isRight?'right':'']">
+   <span class="title" >{{ title }}</span>
+   <span class="desc">{{ desc }}</span>
+   <div class="img" v-if="img">{{ img}}</div>
+   <span class="date">{{ date }}</span>
+  </div>
+</template>
+<script lang='ts' setup>
+import { ref, reactive } from 'vue'
+const props = defineProps({
+    title: {
+        type:String
+    },
+    desc: {
+        type:String
+    },
+    date: {
+        type:String
+    },
+    img: {
+        type:String
+    },
+    isRight:{
+        type: Boolean
+    }
+})
+</script>
+<style scoped lang='less'>
+ .itemContainer{
+     padding: 10px;
+     .title{
+         font-weight: 700;
+         font-size: 18px;
+     }
+     .desc{
+         margin: 5px 0;
+     }
+     .img{
+         width: 300px;
+         height: 200px;
+         background-color: pink;
+         margin: 10px 0;
+     }
+ }
+ .itemContainer.right{
+     padding: 10px;
+     .title, .desc, .date{
+         text-align: end;
+     }
+ }
+</style>
--- src/views/timeLine/index.vue
+++ src/views/timeLine/index.vue
@@ -1,11 +1,71 @@
 <template>
-  <div>
-    时间线组件
-  </div>
+    <div class="timeLine-container">
+        <el-row :gutter="20">
+            <el-col :span="16">
+                <el-timeline>
+                    <el-timeline-item v-for="(item,index) in list" :key="index" :class="[index%2 !== 0?'right':'']" center>
+                        <timeLine v-bind="{...item}" :isRight="index%2 !== 0?true:false"></timeLine>
+                    </el-timeline-item>
+                </el-timeline>
+            </el-col>
+            <el-col :span="8">
+                <div class="grid-content ep-bg-purple" />
+            </el-col>
+        </el-row>
+    </div>
 </template>
 <script lang='ts' setup>
+import timeLine from './components/timeItem.vue'
 import { ref, reactive } from 'vue'
+const list = reactive([
+    {
+        title:'这是文章标题',
+        desc:'这是文章描述',
+        img:'',
+        date:'2023-10-10',
+        id:1
+    },
+    {
+        title:'这是文章标题',
+        desc:'这是文章描述',
+        img:'',
+        date:'2023-10-10',
+        id:2
+    },
+    {
+        title:'这是文章标题',
+        desc:'这是文章描述',
+        img:'',
+        date:'2023-10-10',
+        id:3
+    },
+])
 </script>
 <style scoped lang='less'>
- 
+.timeLine-container {
+    width: 80%;
+    min-height: 100vh;
+    background-color: #fff;
+    margin: 0 auto;
+}
+
+:deep(.el-timeline-item__tail) {
+    left: 50%;
+}
+
+:deep(.el-timeline-item__node) {
+    left: calc(50% - 5px);
+}
+
+:deep(.el-timeline-item__wrapper) {
+    left: calc(50% - 5px);
+    width: 50% !important;
+}
+
+.right {
+
+    :deep(.el-timeline-item__wrapper) {
+        left: calc(0% - 45px)!important;
+    }
+}
 </style>
