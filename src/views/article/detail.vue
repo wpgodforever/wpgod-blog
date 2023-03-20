fa808d0c010e0dac9a902c2f24bff460dfc3f241
wpgodforever <997587729@qq.com>
2023-03-20 11:12:51 +0800

    [add]文章详情更新时间发布时间

--- src/views/article/detail.vue
+++ src/views/article/detail.vue
@@ -2,6 +2,10 @@
   <div class="editorBox flex-col">
     <div class="editorBox-detail">
       <MdEditor v-model.trim="text" previewOnly/>
+      <div class="time flex-align">
+         <span>{{ '该文章发布于' + timeTransform(createdAt)  }}</span>
+         <span>{{ '更新于' + timeTransform(updatedAt) }}</span>
+      </div>
     </div>
     <!-- 评论区域 -->
     <div class="commentBox">
@@ -14,17 +18,22 @@ import { ref, reactive } from 'vue'
 import MdEditor from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 import { useRoute } from 'vue-router';
+import { timeTransform } from '@/lib/utils.js'
 import {
   articleDetailFn
 } from '@/api/article/index'
 import myComment from '@/components/comment/index.vue'
 const route = useRoute()
 let text = ref('')
+let updatedAt = ref('')
+let createdAt = ref('')
 const list = ref([])
 const getCommentList = () => {
   articleDetailFn({_id:route.params.id}).then(res => {
   text.value = res.data[0].text
   list.value = res.data[0].coms
+  updatedAt.value = res.data[0].updatedAt
+  createdAt.value = res.data[0].createdAt
 })
 }
 getCommentList()
@@ -42,8 +51,13 @@ const replyDeepSuccess = () => {
  .editorBox{
    align-items: center;
    background-color: #fff;
+   min-height: calc(100vh - 59px);
   &-detail{
     width: 80%;
+    .time{
+      justify-content: space-between;
+      margin-bottom: 20px;
+    }
   }
   .commentBox{
     width: 80%;
