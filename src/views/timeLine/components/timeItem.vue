4d9d4c21357b683627ae7d4eafbaa6e1650a82e6
wpgodforever <997587729@qq.com>
2023-03-21 16:19:00 +0800

    [add]时间线图片样式优化

--- src/views/timeLine/components/timeItem.vue
+++ src/views/timeLine/components/timeItem.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="itemContainer flex-col" :class="[isRight?'right':'']">
-   <span class="title">{{ title }}</span>
-   <span class="desc" :style="{
-       color: colorInfo[Math.floor(Math.random() * 5 + 1)]
-   }">{{ desc }}</span>
-   <div class="img" v-if="cover">
-    <img :src="cover" alt="" srcset="">
-</div>
-   <span class="date">{{ timeTransform(createdAt) }}</span>
-  </div>
+    <div class="itemContainer flex-col" :class="[isRight ? 'right' : '']">
+        <span class="title">{{ title }}</span>
+        <span class="desc" :style="{
+            color: colorInfo[Math.floor(Math.random() * 5 + 1)]
+        }">{{ desc }}</span>
+        <div class="img" v-if="cover">
+            <img :src="cover" alt="" srcset="">
+        </div>
+        <span class="date">{{ timeTransform(createdAt) }}</span>
+    </div>
 </template>
 <script lang='ts' setup>
 import { timeTransform } from '@/lib/utils.js'
 import { ref, reactive } from 'vue'
 const props = defineProps({
     title: {
-        type:String
+        type: String
     },
     desc: {
-        type:String
+        type: String
     },
     createdAt: {
-        type:String
+        type: String
     },
     cover: {
-        type:String
+        type: String
     },
-    isRight:{
+    isRight: {
         type: Boolean
     }
 })
@@ -39,31 +39,41 @@ const colorInfo = reactive({
 })
 </script>
 <style scoped lang='less'>
- .itemContainer{
-     padding: 10px;
-     border-bottom: 1px dashed #ccc;
-     .title{
-         font-weight: 700;
-         font-size: 18px;
-     }
-     .desc{
-         margin: 5px 0;
-     }
-     .img{
-         width: 300px;
-         height: 200px;
-         margin: 10px 0;
-         object-fit: cover;
-         overflow: hidden;
-     }
-     .date{
-         color: #6ca189;
-     }
- }
- .itemContainer.right{
-     padding: 10px;
-     .title, .desc, .date{
-         text-align: end;
-     }
- }
-</style>
+.itemContainer {
+    padding: 10px;
+    border-bottom: 1px dashed #ccc;
+
+    .title {
+        font-weight: 700;
+        font-size: 18px;
+    }
+
+    .desc {
+        margin: 5px 0;
+    }
+
+    .img {
+        width: 300px;
+        height: 200px;
+        overflow: hidden;
+        img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+
+    .date {
+        color: #6ca189;
+    }
+}
+
+.itemContainer.right {
+    padding: 10px;
+
+    .title,
+    .desc,
+    .date {
+        text-align: end;
+    }
+}</style>
