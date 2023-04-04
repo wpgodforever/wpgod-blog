ddff13fcf8c9010d87c16933fa2fbc5eca471e0b
wpgodforever <997587729@qq.com>
2023-04-04 19:01:41 +0800

    [add]小球堆积案例

--- src/views/demo/components/demoCanvas/test.vue
+++ src/views/demo/components/demoCanvas/test.vue
@@ -7,121 +7,98 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-const canvas = ref<HTMLCanvasElement>();
-interface ballColorConfigInterface {
-  r: number,
-  g: number,
-  b: number,
+// 获取canvas画布
+const canvas = ref<HTMLCanvasElement | null>(null);
+
+// 获取上下文
+let ctx = null
+onMounted(()=>{
+   ctx= canvas.value.getContext('2d')
+
+const ballRadius = 20 // 小球半径
+const ballColor = '#FF0000' // 小球颜色
+const gravity = 0.5 // 重力加速度
+
+let ballList = [] // 小球列表
+
+// 绘制小球
+function drawBall(ball) {
+  ctx.beginPath()
+  ctx.arc(ball.x, ball.y, ballRadius, 0, 2 * Math.PI)
+  ctx.fillStyle = ballColor
+  ctx.fill()
 }
-class Boom {
-  canvasDom:HTMLCanvasElement;
-  ctx:CanvasRenderingContext2D;
-  ballList: Array<Ball> = [];//存放每次点击出现的小球数组
-  ballNum: number;//每次点击出现多少个小球
-  ballColorConfig: Array<ballColorConfigInterface>;
-  constructor(canvasDom:HTMLCanvasElement,ballNum: number, ballColorConfig?: Array<ballColorConfigInterface>) {
-    this.ballNum = ballNum
-    this.ballColorConfig = ballColorConfig || [//一个颜色数组，其中包含了四种不同的颜色，用于作为粒子的颜色。可以根据需求调整颜色
-      {
-        r: 36,
-        g: 18,
-        b: 42
-      },
-      {
-        r: 78,
-        g: 36,
-        b: 42
-      },
-      {
-        r: 252,
-        g: 178,
-        b: 96
-      },
-      {
-        r: 253,
-        g: 238,
-        b: 152
-      }
-    ]
-    this.canvasDom = canvasDom
-    this.ctx = canvasDom.getContext('2d')
-    this.init(canvasDom)
+
+// 更新小球位置和速度
+// 更新小球位置和速度
+function updateBall(ball) {
+  ball.vy += gravity // 更新小球在垂直方向上的速度
+  ball.y += ball.vy // 更新小球的垂直位置
+  if (ball.y + ballRadius > canvas.value.height) { // 如果小球触底
+    ball.y = canvas.value.height - ballRadius // 将小球位置重置到画布底部
+    ball.vy = -ball.vy * 0.8 // 反弹，并减小速度（弹性损失）
   }
-  init(canvasDom){
-    this.animate()
-    canvasDom.addEventListener('click',(e) => {
-      const { offsetX, offsetY } = e;
-      for(let i = 0;i<this.ballNum; i++){
-        const color = this.ballColorConfig[Math.floor(Math.random()*this.ballColorConfig.length)]
-        this.ballList.push(new Ball(canvasDom,offsetX,offsetY,color))
+  // 判断小球与其他小球是否相撞
+  for (let i = 0; i < ballList.length; i++) {
+    const otherBall = ballList[i]
+    if (otherBall !== ball) { // 排除自己
+      const dx = otherBall.x - ball.x
+      const dy = otherBall.y - ball.y
+      const distance = Math.sqrt(dx * dx + dy * dy)
+      if (distance <= ballRadius * 2) { // 相撞了
+        // 计算弹开的方向和力度
+        const angle = Math.atan2(dy, dx)
+        const force = (ballRadius * 2 - distance) * 0.1
+        // 更新自己的位置和速度
+        ball.x -= Math.cos(angle) * force
+        ball.y -= Math.sin(angle) * force
+        ball.vy = -ball.vy * 0.8
+        // 更新其他小球的位置和速度
+        otherBall.x += Math.cos(angle) * force
+        otherBall.y += Math.sin(angle) * force
+        otherBall.vy = -otherBall.vy * 0.8
       }
-    })
-  }
-  animate(){
-    window.requestAnimationFrame(() => {
-      this.ctx.clearRect(0,0,this.canvasDom.width,this.canvasDom.height)
-      this.ballList.forEach(v => {
-        v.updateBall()
-        v.draw()
-      })
-      this.animate()
-    })
+    }
   }
 }
 
-class Ball {
-  canvasDom:HTMLCanvasElement;
-  ctx:CanvasRenderingContext2D;
-  x:number;//小球X轴位置
-  y:number;//小球Y轴位置
-  speed:number;//小球移动速度
-  deg:number;//小球运动角度
-  r:number;//小球半径
-  ballColor:ballColorConfigInterface;//小球颜色
-  constructor(canvasDom:HTMLCanvasElement,x:number,y:number,ballColor?:ballColorConfigInterface){
-    this.canvasDom = canvasDom
-    this.x = x
-    this.y = y
-    this.ballColor = ballColor || {
-        r: 36,
-        g: 18,
-        b: 42
-      }
-    this.r = this.randomR()
-    this.randomSpeed()
-    this.randomDeg()
-    this.ctx = canvasDom.getContext('2d')
-    this.draw()
+
+// 添加小球
+function addBall(e) {
+  const x = e.clientX - canvas.value.offsetLeft
+  const y = e.clientY - canvas.value.offsetTop
+  const ball = {
+    x: x,
+    y: y,
+    vy: Math.random() * -10, // 随机生成小球在垂直方向上的初速度
   }
-  draw(){
-    this.ctx.beginPath()
-    this.ctx.fillStyle = `rgba(${this.ballColor.r},${this.ballColor.g},${this.ballColor.b},255)`
-    this.ctx.arc(this.x,this.y,this.r,0,360*(Math.PI/180))
-    this.ctx.fill()
-    this.ctx.closePath()
-  }
-  updateBall(){
-    let a = 180 - (this.deg + 90);
-    this.deg > 0 && this.deg < 180
-      ? (this.x += Math.cos(this.deg)*this.speed)
-      : (this.x -= Math.cos(this.deg)*this.speed);
-      this.deg > 90 && this.deg < 270
-      ? (this.y += this.speed * Math.sin(a))
-      : (this.y -= this.speed * Math.sin(a));
-  }
-  randomSpeed(){//小球随机速度
-    this.speed = Math.random() * 15
-  }
-  randomDeg(){//小球随机运动角度
-    this.deg = Math.round(Math.random() * 360)
-  }
-  randomR(){//小球随机半径
-    return Math.floor(Math.random() * 5) + 5
-  }
+  ballList.push(ball)
 }
-onMounted(() => {
-  new Boom(canvas.value,40)
-});
+
+// 渲染场景
+function render() {
+  ctx.clearRect(0, 0, canvas.value.width, canvas.value.height)
+
+// 遍历小球列表，依次更新和绘制每一个小球
+for (let i = 0; i < ballList.length; i++) {
+const ball = ballList[i]
+updateBall(ball)
+drawBall(ball)
+}
+}
+
+// 每帧动画的主函数
+function animate() {
+requestAnimationFrame(animate)
+render()
+}
+
+// 绑定事件监听器
+canvas.value.addEventListener('click', addBall)
+
+// 启动动画
+animate()
+})
 </script>
 
 <style lang="less" scoped>
