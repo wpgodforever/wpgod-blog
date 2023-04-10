7cbaeb65856f91be1eaa850fe69d2112fc8864d2
wpgodforever <997587729@qq.com>
2023-04-10 16:28:58 +0800

    滚动条样式优化

--- src/components/starBg/index.vue
+++ src/components/starBg/index.vue
@@ -53,10 +53,10 @@ const start = () => {
     position: absolute;
     width: calc(100%);
     height: 100%;
-    left: -10px;
+    // left: -10px;
     /* 在容器中心的渐变，从红色开始，变成蓝色，最后变成绿色 */
     background-image: radial-gradient(ellipse at top, #050b12 0%, #181b25 100%);
-    z-index: 1;
+    z-index: -1;
     overflow: hidden;
     .star {
         width: 1px;
